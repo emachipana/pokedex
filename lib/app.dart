74f9fe4f038e382d8bbdff7b45c6da6c32df25c5
emachipana <emachipana0102@gmail.com>
2025-01-30 11:08:22 -0500

    setup project

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PokedexApp extends StatelessWidget {
+  const PokedexApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/app.dart';
+
+void main() {
+  runApp(const PokedexApp());
+}
